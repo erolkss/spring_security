ec14421094a34b54de5fabb09048d20181688076
Lucas Lima <lucaserolima@gmail.com>
2024-01-30 16:30:15 -0400

    Updating the 'getAuthorities' method.

--- src/main/java/br/com/example/springsecurity/entity/User.java
+++ src/main/java/br/com/example/springsecurity/entity/User.java
@@ -3,9 +3,11 @@ package br.com.example.springsecurity.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
@@ -22,7 +24,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
