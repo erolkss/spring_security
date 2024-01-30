a374acb11fc2380c77a68f32244ba45fb0c7b7e1
Lucas Lima <lucaserolima@gmail.com>
2024-01-30 16:50:16 -0400

    Creating the methods that will generate and decode our security key Token

--- src/main/java/br/com/example/springsecurity/service/impl/JWTServiceImpl.java
+++ src/main/java/br/com/example/springsecurity/service/impl/JWTServiceImpl.java
@@ -1,7 +1,28 @@
 package br.com.example.springsecurity.service.impl;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JWTServiceImpl {
+    private String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 40))
+                .signWith(getSignKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private Key getSignKey(){
+        byte[] key = Decoders.BASE64.decode("413F4428472B4B625065536856605970337336763979244226452948404D6351");
+        return Keys.hmacShaKeyFor(key);
+    }
+
 }
