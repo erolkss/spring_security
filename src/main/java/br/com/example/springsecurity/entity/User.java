e8e69586a544fdfd0b2c20a87f33cae57be3ef6d
Lucas Lima <lucaserolima@gmail.com>
2024-01-30 16:28:33 -0400

    Implementing the 'UserDetails' Interface in the 'User' Entity

--- src/main/java/br/com/example/springsecurity/entity/User.java
+++ src/main/java/br/com/example/springsecurity/entity/User.java
@@ -2,11 +2,15 @@ package br.com.example.springsecurity.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -16,4 +20,33 @@ public class User {
     private String password;
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
