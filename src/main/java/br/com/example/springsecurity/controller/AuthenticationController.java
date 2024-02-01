869e03a219288799fefded2b1a9073fe47694050
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 18:59:06 -0400

    Mentioning the post method for creating a user in 'AuthenticationController'

--- src/main/java/br/com/example/springsecurity/controller/AuthenticationController.java
+++ src/main/java/br/com/example/springsecurity/controller/AuthenticationController.java
@@ -1,6 +1,12 @@
 package br.com.example.springsecurity.controller;
 
+import br.com.example.springsecurity.dto.SignUpRequest;
+import br.com.example.springsecurity.entity.User;
+import br.com.example.springsecurity.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signUp(@RequestBody SignUpRequest signUpRequest){
+        return ResponseEntity.ok(authenticationService.signUp(signUpRequest));
+
+    }
 }
