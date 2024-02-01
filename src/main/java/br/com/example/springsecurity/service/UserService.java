da40ddc9df521be1d1fcdaab9e57ece1794a0a60
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 16:50:07 -0400

    calling the 'userDetailsService' method

--- src/main/java/br/com/example/springsecurity/service/UserService.java
+++ src/main/java/br/com/example/springsecurity/service/UserService.java
@@ -1,4 +1,7 @@
 package br.com.example.springsecurity.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 public interface UserService {
+    UserDetailsService userDetailsService();
 }
