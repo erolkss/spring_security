7654310b0230f5d1384aba9c80901cab2dd78f6a
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 17:54:20 -0400

    Add new Annotation '@RequiredArgsConstructor'

--- src/main/java/br/com/example/springsecurity/controller/AuthenticationController.java
+++ src/main/java/br/com/example/springsecurity/controller/AuthenticationController.java
@@ -1,9 +1,11 @@
 package br.com.example.springsecurity.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 }
