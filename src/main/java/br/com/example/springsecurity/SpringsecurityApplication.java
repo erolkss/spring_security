3323f9df2697f4bd54ef296a70f968dc6a8d27ac
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 19:06:23 -0400

    Create a new User admin for default

--- src/main/java/br/com/example/springsecurity/SpringsecurityApplication.java
+++ src/main/java/br/com/example/springsecurity/SpringsecurityApplication.java
@@ -1,13 +1,37 @@
 package br.com.example.springsecurity;
 
+import br.com.example.springsecurity.entity.Role;
+import br.com.example.springsecurity.entity.User;
+import br.com.example.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringsecurityApplication {
+public class SpringsecurityApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
 
+    public void run(String... args) {
+        User adminAccount = userRepository.findByRole(Role.ADMIN);
+        if (null == adminAccount){
+            User user = new User();
+
+            user.setEmail("admin@gmail.com");
+            user.setFirstName("admin");
+            user.setSecondName("admin");
+            user.setRole(Role.ADMIN);
+            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+
+            userRepository.save(user);
+
+        }
+    }
 }
