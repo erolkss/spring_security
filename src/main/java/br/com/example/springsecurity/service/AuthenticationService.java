524f5df2e951af286e89c173409fdb36e51297a6
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 18:55:19 -0400

    Mentioning 'signUp' method in 'AuthenticationService' for creating new user

--- src/main/java/br/com/example/springsecurity/service/AuthenticationService.java
+++ src/main/java/br/com/example/springsecurity/service/AuthenticationService.java
@@ -1,4 +1,9 @@
 package br.com.example.springsecurity.service;
 
+import br.com.example.springsecurity.dto.SignUpRequest;
+import br.com.example.springsecurity.entity.User;
+
 public interface AuthenticationService {
+
+    User signUp(SignUpRequest signUpRequest);
 }
