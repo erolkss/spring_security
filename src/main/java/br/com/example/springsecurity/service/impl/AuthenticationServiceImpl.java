52d7985ba0abdb17a94bc90e79747074363791e0
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 19:33:06 -0400

    Creating a method 'signIn' for generateToken or refreshToken in 'AuthenticationServiceImpl'

--- src/main/java/br/com/example/springsecurity/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/br/com/example/springsecurity/service/impl/AuthenticationServiceImpl.java
@@ -1,13 +1,20 @@
 package br.com.example.springsecurity.service.impl;
 
+import br.com.example.springsecurity.dto.JwtAuthenticationResponse;
+import br.com.example.springsecurity.dto.SignInRequest;
 import br.com.example.springsecurity.dto.SignUpRequest;
 import br.com.example.springsecurity.entity.Role;
 import br.com.example.springsecurity.entity.User;
 import br.com.example.springsecurity.repository.UserRepository;
 import br.com.example.springsecurity.service.AuthenticationService;
+import br.com.example.springsecurity.service.JWTService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +22,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
 
     public User signUp(SignUpRequest signUpRequest) {
         User user = new User();
@@ -25,5 +34,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
 
         return userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse signIn(SignInRequest signInRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getEmail(), signInRequest.getPassword()));
+
+        var user = userRepository.findByEmail(signInRequest.getEmail()).orElseThrow(() -> new IllegalArgumentException("Invalid Email or Password"));
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
+
+        JwtAuthenticationResponse jwtAuthenticationResponse = new JwtAuthenticationResponse();
+
+        jwtAuthenticationResponse.setToken(jwt);
+        jwtAuthenticationResponse.setToken(refreshToken);
+        return jwtAuthenticationResponse;
     }
 }
