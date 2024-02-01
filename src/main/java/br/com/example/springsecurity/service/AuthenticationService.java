e5884e6c90565ccaf7ca235e2c957caa1cdb4864
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 19:34:01 -0400

    Mentioning a method 'signIn' in 'AuthenticationService'

--- src/main/java/br/com/example/springsecurity/service/AuthenticationService.java
+++ src/main/java/br/com/example/springsecurity/service/AuthenticationService.java
@@ -1,9 +1,12 @@
 package br.com.example.springsecurity.service;
 
+import br.com.example.springsecurity.dto.JwtAuthenticationResponse;
+import br.com.example.springsecurity.dto.SignInRequest;
 import br.com.example.springsecurity.dto.SignUpRequest;
 import br.com.example.springsecurity.entity.User;
 
 public interface AuthenticationService {
 
     User signUp(SignUpRequest signUpRequest);
+    JwtAuthenticationResponse signIn(SignInRequest signInRequest);
 }
