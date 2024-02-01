e43c5bf3ba61b3cd0941a9b69e6155ca065a765b
Lucas Lima <lucaserolima@gmail.com>
2024-02-01 18:53:53 -0400

    Created the 'signUp' method in 'AuthenticationServiceImpl' to create a new user

--- src/main/java/br/com/example/springsecurity/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/br/com/example/springsecurity/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,8 @@
 package br.com.example.springsecurity.service.impl;
 
+import br.com.example.springsecurity.dto.SignUpRequest;
+import br.com.example.springsecurity.entity.Role;
+import br.com.example.springsecurity.entity.User;
 import br.com.example.springsecurity.repository.UserRepository;
 import br.com.example.springsecurity.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
@@ -12,4 +15,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    public User signUp(SignUpRequest signUpRequest) {
+        User user = new User();
+        user.setEmail(signUpRequest.getEmail());
+        user.setFirstName(signUpRequest.getFirstName());
+        user.setSecondName(signUpRequest.getLastName());
+        user.setRole(Role.USER);
+        user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
+
+        return userRepository.save(user);
+    }
 }
